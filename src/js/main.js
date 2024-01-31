8bf1e3cca32dd57c15c2689a3773f141372f0833
Epineux <martinlecontehelloworld@gmail.com.noreply.github.com>
2024-01-31 21:19:17 +0900

    Add the HTML and the general style

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import "../css/reset.css";
+import "../css/style.css";
+
+// const icon = document.querySelector(".theme-icon");
+
+// icon.addEventListener('click', ()=> {
+//   icon.style.transform = icon.style.transform === 'rotate(180deg)' ? 'rotate(0deg)' : 'rotate(180deg)';
+// });
